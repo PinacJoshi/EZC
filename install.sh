52d6f23409230735ab95eb9982a5da9a3fcdef89
PinacJoshi <88838102+PinacJoshi@users.noreply.github.com>
2023-01-10 17:17:52 +0530

    Revert "Added Instruciton for error"
    This reverts commit a420cdcb1897691202bfb622670cb868cc79fc0f.

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 #Run Using $sudo ./install.sh
 
 if [ "$EUID" -ne 0 ] # Check to see if user is giving root privileges
-  then echo -e "\e[1;31m * Please run as root \n * Use \"sudo ./install.sh\""
+  then echo -e "\e[1;31m * Please run as root"
   exit
 fi
 
 echo -e "\e[1;32m * Installing EZC"
 
-sudo chmod +x ezc 
-sudo cp -n ezc /usr/bin/ 
+sudo chmod +x ezc     #Remove Comment After Development is Completed
+sudo cp ezc /usr/bin/ #Remove Comment After Development is Completed
 
 echo -e "\e[1;32m * EZC Installation Completed"
