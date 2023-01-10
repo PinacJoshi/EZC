69ae868abe742312dd6669e7046190b9711f469e
PinacJoshi <88838102+PinacJoshi@users.noreply.github.com>
2023-01-10 17:16:54 +0530

    Added Instruciton for error

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 #Run Using $sudo ./install.sh
 
 if [ "$EUID" -ne 0 ] # Check to see if user is giving root privileges
-  then echo -e "\e[1;31m * Please run as root"
+  then echo -e "\e[1;31m * Please run as root \n * Use \"sudo ./install.sh\""
   exit
 fi
 
 echo -e "\e[1;32m * Installing EZC"
 
-sudo chmod +x ezc     #Remove Comment After Development is Completed
-sudo cp ezc /usr/bin/ #Remove Comment After Development is Completed
+sudo chmod +x ezc 
+sudo cp -n ezc /usr/bin/ 
 
 echo -e "\e[1;32m * EZC Installation Completed"
